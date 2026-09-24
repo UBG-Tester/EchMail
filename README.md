89f59cba3fb4a0589bb0d0edc48e7a3a825e6ace
UBG-Tester <ubgtester@atomicmail.io>
2026-09-23 20:25:33 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# EchMail
-mail
+# Echmail 📧🍍😎
+
+<div align="center">
+  <img width=100% height="100%" src="https://github-production-user-asset-6210df.s3.amazonaws.com/71232870/242247596-36d6b150-a7ec-4016-bf7b-588d401628d7.png" />
+</div>
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Run your unit tests
+```
+npm run test:unit
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ vue.config.js
@@ -0,0 +1,41 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
+  pwa: {
+    name: 'Echmail design by Hosein Sedaqat',
+    themeColor: "#fff",
+    iconPaths: {
+      favicon32: 'img/icons/favicon.ico',
+      favicon16: 'img/icons/favicon.ico',
+      appleTouchIcon: 'img/icons/apple-icon-72x72.png',
+      msTileImage: 'img/icons/apple-icon-144x144.png'
+    },
+    manifestOptions: {
+      background_color: "#fff",
+      icons: [
+        {
+          src: "img/icons/android-icon-192x192.png",
+          sizes: "192x192",
+          type: "image/png"
+        },
+        {
+          src: "img/icons/ms-icon-310x310.png",
+          sizes: "310x310",
+          type: "image/png"
+        },
+        {
+          src: "img/icons/android-icon-192x192.png",
+          sizes: "192x192",
+          type: "image/png",
+          purpose: "maskable"
+        },
+        {
+          src: "img/icons/ms-icon-310x310.png",
+          sizes: "310x310",
+          type: "image/png",
+          purpose: "maskable"
+        }
+      ]
+    }
+  }
+})
