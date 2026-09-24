6a4d9a16989c8ea9d1472a1153bb8e5484977e30
UBG-Tester <ubgtester@atomicmail.io>
2026-09-23 20:56:11 -0500

    Add files via upload

---
+++ tests/unit/state.spec.js
@@ -0,0 +1,15 @@
+import { setActivePinia, createPinia } from 'pinia';
+
+import { setting } from "@/store/module/setting";
+
+beforeEach(() => {
+    setActivePinia(createPinia())
+})
+
+test('pinia 🍍 state', () => {
+
+    const setting_store = setting();
+
+    expect(setting_store.user_name).toBe('Hosein Sedaqat');
+
+})
